df56c425c77390a692c246d0e166f33dafe2be10
lintopher0315 <lin978@purdue.edu>
2019-11-30 21:13:20 -0500

    edited oauth to redirect nonpremium accounts to spotify premium page

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -100,17 +100,20 @@ app.get('/callback', function(req, res) {
 
         // use the access token to access the Spotify Web API
         request.get(options, function(error, response, body) {
-          console.log(body);
+          if (body.product === 'premium') {
+            res.redirect('http://localhost:3000/home/#' +
+              querystring.stringify({
+                access_token: access_token,
+                refresh_token: refresh_token
+              }));
+          }
+          else {
+            res.redirect('https://www.spotify.com/us/premium/');
+          }
         });
-
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/home/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
       } else {
-        res.redirect('/#' +
+        res.redirect('http://localhost:3000/#' +
           querystring.stringify({
             error: 'invalid_token'
           }));
